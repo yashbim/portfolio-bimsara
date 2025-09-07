6a5f33efed8c65c56b1b636c0d468dcb6918bdbd
yashbim <ybimsara03@gmail.com>
2025-09-07 09:35:19 +0530

    Added contact info and favicon

--- constants/contact.ts
+++ constants/contact.ts
@@ -1,9 +1,20 @@
-export interface ContactLink {
-  label: string;
-  href: string;
-  icon: React.ReactNode;
-}
+import {
+  FaEnvelope,
+  FaLinkedin,
+  FaGithub,
+  FaTwitter,
+  FaInstagram,
+  FaFacebook,
+  FaMedium
+} from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6"; // For the new X (Twitter)
 
-export const CONTACT_EMAIL = "bimsara@example.com";
-export const LINKEDIN_URL = "https://www.linkedin.com/in/your-profile";
-export const GITHUB_URL = "https://github.com/your-username";
+export const CONTACTS = [
+  { name: "Email", href: "mailto:ybimsara03@gmail.com", icon: FaEnvelope },
+  { name: "LinkedIn", href: "https://www.linkedin.com/in/bimsara-madurapperuma-ab6a53232/", icon: FaLinkedin },
+  { name: "GitHub", href: "https://github.com/yashbim", icon: FaGithub },
+  { name: "Medium", href: "https://medium.com/@ybimsara03", icon: FaMedium },
+  { name: "Instagram", href: "https://instagram.com/bimsara_m", icon: FaInstagram },
+  { name: "Facebook", href: "https://www.facebook.com/bimsara.madurapperuma", icon: FaFacebook },
+  { name: "X (Twitter)", href: "https://x.com/Bimsara_M", icon: FaXTwitter },
+];
