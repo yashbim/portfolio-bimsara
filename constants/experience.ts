dfeec668460a7fd9f00f26888ca35a5972433947
yashbim <ybimsara03@gmail.com>
2026-03-01 00:42:43 +0530

    Update work experience section with logos

--- constants/experience.ts
+++ constants/experience.ts
@@ -2,6 +2,7 @@ export interface WorkExperience {
   role: string;
   company: string;
   period: string;
+  icon: string;
   summary: string;
 }
 
@@ -10,6 +11,7 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
     role: "Trainee Software Engineer",
     company: "hSenid Mobile Solutions",
     period: "June 2023 - August 2024",
+    icon:"/companies/hsenid.png",
     summary:
     "",
       // "Worked on integrating office suite tools, and creating reporting dashboards using Grafana for telecommunication API management.",
@@ -18,13 +20,15 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
     role: "Associate Business Analyst",
     company: "hSenid Mobile Solutions",
     period: "August 2025 - Present",
+    icon:"/companies/hsenid.png",
     summary:
       "",
   },
   {
     role: "Tech Lead",
     company: "DevTeam - AIESEC in Sri Lanka",
-    period: "August 2025 - Present",
+    period: "January 2026 - Present",
+    icon:"/companies/AIESEC-Human-Blue.png",
     summary:
       "",
   },
@@ -32,6 +36,7 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
     role: "Developer",
     company: "Maverick Intelligence",
     period: "June 2025 - October 2025 - (Part Time)" ,
+    icon:"/companies/maverick.png",
     summary:
     "",
       // "Built stock market prediction applications, and worked on ERP systems for real estate industries.",
