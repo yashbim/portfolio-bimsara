e5b97362192227dedec98b48ec734fd179154fc7
yashbim <ybimsara03@gmail.com>
2026-05-03 01:00:17 +0530

    Added google analytics (1)

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,10 @@ export default function RootLayout({
         {children}
       </body>
       <Analytics />
+      {process.env.NEXT_PUBLIC_GA_ID && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />
+      )}
     </html>
   );
 }
+
